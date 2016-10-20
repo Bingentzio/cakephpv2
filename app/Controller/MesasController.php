117e9398daf35bba41034db67293aff4527b2cc4
bingen <bingen.glez@gmail.com>
2016-10-20 18:01:09 +0200

    Mahai berriak gehitu

--- app/Controller/MesasController.php
+++ app/Controller/MesasController.php
@@ -1,6 +1,28 @@
 <?php
 class MesasController extends AppController
 {
-    
+    public $helpers = array('Html','Form','Time');
+    public $components = array('Flash');
+
+    public function index()
+    {
+        $this->set('mesas', $this->Mesa->find('all'));
+    }
+
+    public function nuevo()
+    {
+        if($this->request->is('post'))
+        {
+            $this->Mesa->create();
+            if($this->Mesa->save($this->request->data))
+            {
+                $this->Flash->Success('La mesa ha sido creada');
+                return $this->redirect(array('action'=>'index'));
+            }
+            $this->Flash->Error('No se pudo crear mesa');
+        }
+        $meseros = $this->Mesa->Mesero->find('list', array('fields' => array('id','nombre_completo')));
+        $this->set('meseros',$meseros);
+    }
 }
 ?>
